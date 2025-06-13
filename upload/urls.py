dbd0871e1d371bdf2f7f60e945be5641b6b3b8d5
fxkim <frkim@live.fr>
2025-06-13 11:51:07 +0200

    Refactor URL routing to include upload URLs in the main urlpatterns

--- upload/urls.py
+++ upload/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='upload_page'),
     path('upload/', views.upload_file, name='upload_file'),
     path('translate/', views.translate_documents, name='translate_documents'),
     path('download/<str:filename>/', views.download_file, name='download_file'),
-    path('files/', views.list_user_files, name='list_user_files'),
+    path('api/files/', views.list_user_files, name='list_user_files'),
+    # Health check endpoints
     path('health/', views.health_check, name='health_check'),
     path('ready/', views.readiness_check, name='readiness_check'),
+    # Storage test endpoint
     path('test-azure-storage/', views.test_azure_storage, name='test_azure_storage'),
 ]
